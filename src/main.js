498c7fe217298d690eb8a6c84a7c7321dd65d5cc
Aiman Josefsson <aiman.josefsson@ispy.se>
2017-07-30 00:22:28 +0200

    MenuState stuff

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ class Game {
     this.renderer = autoDetectRenderer({
       width: 800,
       height: 600,
+      transparent: true
     })
 
     this.states = new StateMachine()
@@ -19,7 +20,7 @@ class Game {
     this.states.addState('menu', new State.MenuState(this))
     this.states.addState('game', new State.GameState(this))
 
-    this.states.setState('game')
+    this.states.setState('menu')
   }
 
   run () {
--- src/states/MenuState.js
+++ src/states/MenuState.js
@@ -1,6 +1,47 @@
 import { State } from 'core/state'
+import { ViewPort } from 'core/gfx'
+import ECS from 'ecs'
+import { Container } from 'pixi'
 
 
 export default class GameState extends State {
+	constructor (game) {
+		super()
+
+		this.renderer = game.renderer
+
+		this.g = new Container()
+
+
+		const clickHere = new PIXI.Text('Click to Start Game!', new PIXI.TextStyle({fontSize: 48, stroke: '#4aff50', fill: ['#ff00ff', '#00ff99']}))
+		clickHere.x = this.renderer.width/2
+		clickHere.y = this.renderer.height/3
+
+		clickHere.anchor.x = 0.5
+
+		const credits = new PIXI.Text('Credits:\nTroy Erem\nAiman Josefsson\nJonathan Nguyen\nLinus Wahlgren\nMax Wihlborg')
+		credits.x = this.renderer.width/2
+		credits.y = this.renderer.height/2
+		credits.anchor.x = 0.5
+
+		const creditsISPY = new PIXI.Text('Special thanks to ISPY GROUP for letting us hack at their office!')
+		creditsISPY.x = this.renderer.width/2
+		creditsISPY.y = 9*this.renderer.height/10
+		creditsISPY.anchor.x = 0.5
+
+		this.g.addChild(clickHere)
+		this.g.addChild(credits)
+		this.g.addChild(creditsISPY)
+
+		document.body.onclick = () => {
+			game.states.setState('game')
+		}
+	}
+
+	tick() {
+		this.renderer.clear(0x556677)
+		this.renderer.render(this.g)
+
+	}
 }
 
