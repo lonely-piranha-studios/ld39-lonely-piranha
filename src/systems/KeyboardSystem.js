888728993c634ea72237582f65e87ac5c0958aba
pinn3 <iam@lacking.education>
2017-07-30 00:18:52 +0200

    Add facingDirection for animation update

--- src/systems/KeyboardSystem.js
+++ src/systems/KeyboardSystem.js
@@ -4,16 +4,19 @@ import { System } from 'yagl-ecs'
 export default class KeyboardSystem extends System {
 
   test (entity) {
-    return ['physic', 'keyboard'].every(comp => !!entity.components[comp])
+    return ['physic', 'keyboard', 'shape'].every(comp => !!entity.components[comp])
   }
 
   update (entity) {
-    const { physic, keyboard } = entity.components
+    const { physic, keyboard, shape } = entity.components
     const input = keyboard.getState()
     const speed = 3
 
+    shape.previousFacingDirection = shape.facingDirection
     physic.vel.x = speed * (input.down('right') - input.down('left'))
+    shape.facingDirection = physic.vel.x === 0 ? shape.facingDirection : (physic.vel.x < 0 ? 'west' : 'east')
     physic.vel.y = speed * (input.down('down') - input.down('up'))
+    shape.facingDirection = physic.vel.y === 0 ? shape.facingDirection : (physic.vel.y < 0 ? 'north' : 'south')
   }
 
 }
--- src/systems/RenderingSystem.js
+++ src/systems/RenderingSystem.js
@@ -18,10 +18,12 @@ export default class RenderingSystem extends System {
   enter (entity) {
     const { pos, shape } = entity.components
 
-    const g = new AnimatedSprite(this.sprite.getFrameSet('character/run-east', 4))
+    const textures = this.sprite.getFrameSet(`character/run-${shape.facingDirection}`, 4)
+    console.log(textures)
+    const g = new AnimatedSprite(textures)
     g.x = pos.x
     g.y = pos.y
-    g.anchor.set(0.5);
+    g.anchor.set(0, 0.5);
     g.animationSpeed = 0.1;
     g.play()
 
@@ -42,6 +44,11 @@ export default class RenderingSystem extends System {
     const { pos, shape } = entity.components
     shape.graphic.x = pos.x
     shape.graphic.y = pos.y
+    if (shape.facingDirection !== shape.previousFacingDirection) {
+      const textures = this.sprite.getFrameSet(`character/run-${shape.facingDirection}`, 4)
+      shape.graphic.textures = textures
+      shape.graphic.play()
+    }
   }
 
 }
