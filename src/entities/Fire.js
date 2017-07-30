0bd5ca33ac6879b9824499c08ec2b8064d1553fd
Troy <eremtroy@gmail.com>
2017-07-30 23:19:43 +0200

    Add fire

---
+++ src/entities/Fire.js
@@ -0,0 +1,28 @@
+import { Entity } from 'ecs'
+import { Sprite, Texture } from 'pixi'
+
+import Component from 'components'
+
+
+export default class Fire {
+
+  static create (options) {
+    const o = new Entity(null, [
+      Component.Position,
+      Component.Shape,
+      Component.Sprite,
+    ])
+
+    o.updateComponents({
+      pos: { x: options.x, y: options.y },
+      shape: { width: options.w, height: options.h },
+      sprite: {
+        namespace: `tiles/${options.type}`,
+        animationState: options.data.state,
+        animationSpeed: options.data.animationSpeed,
+      },
+    })
+    return o
+  }
+
+}
