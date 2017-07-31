cc80079b4f6bd7c03c7ab525891dee1993dcc7c6
pinn3 <iam@lacking.education>
2017-07-31 05:59:03 +0200

    Add sick-ass animation

--- src/states/GameOverState.js
+++ src/states/GameOverState.js
@@ -1,16 +1,14 @@
 import { State } from 'core/state'
 import { ViewPort } from 'core/gfx'
 import ECS from 'ecs'
-import { Container, Text, TextStyle, Graphics } from 'pixi'
+import { Container,Text, TextStyle, Graphics } from 'pixi'
 
 
 export default class GameOverState extends State {
   constructor (game) {
     super()
 
-    this.game = game
     this.renderer = game.renderer
-
     this.container = new Container()
 
     const background = new Graphics()
@@ -18,11 +16,14 @@ export default class GameOverState extends State {
     background.drawRect(0, 0, game.renderer.width, game.renderer.height)
     this.container.addChild(background)
 
-    const message = new Text('u ded son', new TextStyle({ fontSize: 48, fill: '#ff0000' }))
-    message.x = this.renderer.width/2
-    message.y = this.renderer.height/2
-    message.anchor.x = 0.5
-    this.container.addChild(message)
+    this.message = new Text('u ded son', new TextStyle({ fontSize: 168, fill: '#ff0000' }))
+    this.message.x = this.renderer.width/2
+    this.message.y = this.renderer.height/2
+    this.message.width = this.message.width * 50
+    this.message.height = this.message.width
+    this.message.anchor.x = 0.5
+    this.message.anchor.y = 0.5
+    this.container.addChild(this.message)
   }
 
   tick() {
@@ -31,6 +32,10 @@ export default class GameOverState extends State {
   }
 
   update () {
+    if (this.message.scale.x > 1) {
+      this.message.scale.x -= this.message.scale.x / 10
+      this.message.scale.y = this.message.scale.x
+    }
   }
 }
 
